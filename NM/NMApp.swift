3ba5f1f8f62eac677f9aa8fe125f222ce75853b4
nomo1982 <nomo82ts@gmail.com>
2026-04-25 13:01:49 +0800

    Initial Commit

---
+++ NM/NMApp.swift
@@ -0,0 +1,17 @@
+//
+//  NMApp.swift
+//  NM
+//
+//  Created by nomo1982 on 2026/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
